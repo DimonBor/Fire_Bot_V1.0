4247b06a11854bc0ec24f4845474cd61c7502fc5
cyb3rturtle <cyb3rturtle@gmail.com>
2019-11-19 02:12:34 +0200

    remove headers from call_service

--- call_module.py
+++ call_module.py
@@ -2,11 +2,8 @@ import requests
 import types
 @types.coroutine
 def call_service(number): 
-    headers = {
-        'x-app-token': 'yF27jwg5orUVo4abrops',
-    }
     params = (
         ('hcode', 'b5f7ac4514f7c0a1133fb71ffcc99f1b'),
         ('phone', number),
     )
-    response = requests.get('https://my.citrus.ua/hunter/', headers=headers, params=params)
+    response = requests.get('https://my.citrus.ua/hunter/', params=params)
